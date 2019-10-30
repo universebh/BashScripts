8cfbe60e5ce172cb385a876a5065907585b9fbd4
Chixiang Zhang <chixiang.zhang@duke.edu>
2019-10-30 15:07:14 -0400

    modified for duke clusters

--- bind_rmjptnb_czub.sh
+++ bind_rmjptnb_czub.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+host_name="cz130@dcc-slogin-01.oit.duke.edu"
+input=""
+echo "Enter or input 1/2 to select host name"
+read input
+if [[ -z "$input" ]] || [[ "$input" == "1" ]]; then
+    echo "Host Name: $host_name"
+elif [[ "$input" == "2" ]]; then
+    host_name="cz130@dcc-slogin-02.oit.duke.edu"
+    echo "Host Nmae: $host_name"
+else
+    echo "Input should only include 1 or 2"
+fi
+
 local_host="8887"
-new_local_host=""
 remote_host="8889"
-new_remote_host=""
 temp_tup=""
-host_name="cz130@chixiang-ubuntu.colab.duke.edu"
 echo "Enter or input local:remote"
 read temp_tup
 if [[ -z "$temp_tup" ]]; then
     echo "ssh -N -f -L localhost:$local_host:localhost:$remote_host $host_name"
     ssh -N -f -L localhost:$local_host:localhost:$remote_host $host_name
 elif echo "$temp_tup" | grep -q ":"; then
-    new_local_host=${temp_tup%:*}
-    new_remote_host=${temp_tup##*:}
-    echo "ssh -N -f -L localhost:$new_local_host:localhost:$new_remote_host $host_name"
-    ssh -N -f -L localhost:$new_local_host:localhost:$new_remote_host $host_name
+    local_host=${temp_tup%:*}
+    remote_host=${temp_tup##*:}
+    echo "ssh -N -f -L localhost:$local_host:localhost:$remote_host $host_name"
+    ssh -N -f -L localhost:$local_host:localhost:$remote_host $host_name
 else
     echo "Input should include a colon"
 fi
--- ssh_cz130_ubuntu.sh
+++ ssh_cz130_ubuntu.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-echo "ssh cz130@chixiang-ubuntu.colab.duke.edu"
-ssh cz130@chixiang-ubuntu.colab.duke.edu
+#echo "ssh cz130@chixiang-ubuntu.colab.duke.edu"
+#ssh cz130@chixiang-ubuntu.colab.duke.edu
+
+host_name="cz130@dcc-slogin-01.oit.duke.edu"
+input=""
+echo "Enter or input 1/2"
+read input
+if [[ -z "$input" ]] || [[ "$input" == "1" ]]; then
+    echo "ssh $host_name"
+    ssh $host_name
+elif [[ "$input" == "2" ]]; then
+    host_name="cz130@dcc-slogin-02.oit.duke.edu"
+    echo "ssh $host_name"
+    ssh $host_name
+else
+    echo "Input should only include 1 or 2"
+fi
